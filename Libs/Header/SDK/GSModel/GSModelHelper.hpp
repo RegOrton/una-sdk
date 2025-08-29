9096bea3c62cef7cf1bab3c082ecae85ef794dc0
Oleksandr Tymoshenko <oleksandr.tymoshenko@droid-technologies.com>
2025-08-29 20:18:03 +0300

    updated GSModelHelper

--- Libs/Header/SDK/GSModel/GSModelHelper.hpp
+++ Libs/Header/SDK/GSModel/GSModelHelper.hpp
@@ -2,6 +2,7 @@
 #define __GSMODEL_HELPER_HPP__
 
 #include "SDK/GSModel/GSModel.hpp"
+#include "SDK/GSModel/IGUIModel.hpp"
 #include "GSModelEvents/S2GEvents.hpp"
 #include "GSModelEvents/G2SEvents.hpp"
 
