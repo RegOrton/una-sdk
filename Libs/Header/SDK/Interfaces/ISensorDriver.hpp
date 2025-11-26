6a4fc190ec00ea45e382644f01d3eb0994bc9575
Oleksandr Tymoshenko <oleksandr.tymoshenko@droid-technologies.com>
2025-11-26 17:01:28 +0200

    added descriptions to the drivers of the sensor layer

--- Libs/Header/SDK/Interfaces/ISensorDriver.hpp
+++ Libs/Header/SDK/Interfaces/ISensorDriver.hpp
@@ -21,22 +21,22 @@ namespace SDK::Interface
     
 class ISensorDriver {
 public:
-    ISensorDriver() : mPeriod(1000) {}
+    ISensorDriver() : mRefreshPeriod(1000) {}
     virtual ~ISensorDriver() = default;
 
     virtual bool connect(SDK::Interface::ISensorDataListener* listener,
-                            SDK::Interface::IApp*                app = nullptr,
-                            float                                period  = 0,
-                            uint32_t                             latency = 0) = 0;
+                         SDK::Interface::IApp*                app = nullptr,
+                         float                                period  = 0,
+                         uint32_t                             latency = 0) = 0;
 
     virtual void disconnect(SDK::Interface::ISensorDataListener* listener) = 0;
     
     virtual SDK::Sensor::Type getType() const = 0;
 
-    virtual float getPeriod() const { return mPeriod; }
+    virtual float getRefreshPeriod() const { return mRefreshPeriod; }
 
 protected:
-    float mPeriod;
+    float mRefreshPeriod;
 };
 
 } // namespace SDK::Interface
