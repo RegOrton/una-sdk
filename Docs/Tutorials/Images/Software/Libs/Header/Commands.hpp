3ffbdb173453827b9ce4ee37e2f8a346d0806006
avatarsd <avatarsd2@gmail.com>
2026-03-04 22:11:33 +0200

    feat(gui): add image message types and structures
    Add new message types for image list, loading, and metadata communication between GUI and service. Include corresponding structs and wrapper functions to facilitate image handling in the tutorial.

--- Docs/Tutorials/Images/Software/Libs/Header/Commands.hpp
+++ Docs/Tutorials/Images/Software/Libs/Header/Commands.hpp
@@ -8,6 +8,8 @@
 #include "SDK/Kernel/Kernel.hpp"
 
 #include <array>
+#include <string>
+#include <vector>
 
 // Force 4-byte alignment for all message structures
 #pragma pack(push, 4)
@@ -26,6 +28,16 @@ namespace CustomMessage {
      * - Used to route messages to correct handler in GUI
      */
     // constexpr SDK::MessageType::Type HR_VALUES = 0x00000001;
+
+    // Service --> GUI
+    constexpr SDK::MessageType::Type IMAGE_LIST = 0x00000002;
+    constexpr SDK::MessageType::Type IMAGE_LOADED = 0x00000003;
+    constexpr SDK::MessageType::Type IMAGE_METADATA = 0x00000004;
+
+    // GUI --> Service
+    constexpr SDK::MessageType::Type REQUEST_IMAGE_LIST = 0x00000005;
+    constexpr SDK::MessageType::Type SELECT_IMAGE = 0x00000006;
+    constexpr SDK::MessageType::Type REQUEST_IMAGE_METADATA = 0x00000007;
 
     ///////////////////////////////////////
     //// Application custom structures
@@ -48,6 +60,70 @@ namespace CustomMessage {
     //         , trustLevel()
     //     {}
     // };
+
+    struct ImageListMsg : public SDK::MessageBase {
+        std::vector<std::string> filenames;
+
+        ImageListMsg()
+            : SDK::MessageBase(IMAGE_LIST)
+            , filenames()
+        {}
+    };
+
+    struct ImageLoadedMsg : public SDK::MessageBase {
+        std::string filename;
+        bool success;
+
+        ImageLoadedMsg()
+            : SDK::MessageBase(IMAGE_LOADED)
+            , filename()
+            , success(false)
+        {}
+    };
+
+    struct ImageMetadataMsg : public SDK::MessageBase {
+        std::string filename;
+        uint32_t width;
+        uint32_t height;
+        uint32_t fileSize;
+        std::string lastModified;
+        uint32_t renderTimeMs;
+
+        ImageMetadataMsg()
+            : SDK::MessageBase(IMAGE_METADATA)
+            , filename()
+            , width(0)
+            , height(0)
+            , fileSize(0)
+            , lastModified()
+            , renderTimeMs(0)
+        {}
+    };
+
+    // GUI --> Service
+    struct RequestImageListMsg : public SDK::MessageBase {
+        RequestImageListMsg()
+            : SDK::MessageBase(REQUEST_IMAGE_LIST)
+        {}
+    };
+
+    struct SelectImageMsg : public SDK::MessageBase {
+        std::string filename;
+
+        SelectImageMsg()
+            : SDK::MessageBase(SELECT_IMAGE)
+            , filename()
+        {}
+    };
+
+    struct RequestImageMetadataMsg : public SDK::MessageBase {
+        std::string filename;
+
+        RequestImageMetadataMsg()
+            : SDK::MessageBase(REQUEST_IMAGE_METADATA)
+            , filename()
+        {}
+    };
 
 
     ///////////////////////////////////////
@@ -80,6 +156,39 @@ namespace CustomMessage {
 
         //     return false;
         // }
+
+        bool sendImageList(const std::vector<std::string>& filenames)
+        {
+            if (auto req = SDK::make_msg<CustomMessage::ImageListMsg>(mKernel)) {
+                req->filenames = filenames;
+                return req.send();
+            }
+            return false;
+        }
+
+        bool sendImageLoaded(const std::string& filename, bool success)
+        {
+            if (auto req = SDK::make_msg<CustomMessage::ImageLoadedMsg>(mKernel)) {
+                req->filename = filename;
+                req->success = success;
+                return req.send();
+            }
+            return false;
+        }
+
+        bool sendImageMetadata(const std::string& filename, uint32_t width, uint32_t height, uint32_t fileSize, const std::string& lastModified, uint32_t renderTimeMs)
+        {
+            if (auto req = SDK::make_msg<CustomMessage::ImageMetadataMsg>(mKernel)) {
+                req->filename = filename;
+                req->width = width;
+                req->height = height;
+                req->fileSize = fileSize;
+                req->lastModified = lastModified;
+                req->renderTimeMs = renderTimeMs;
+                return req.send();
+            }
+            return false;
+        }
 
     private:
         const SDK::Kernel& mKernel;
